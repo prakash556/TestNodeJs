210d31d2af95c08835f3581b1676a52a410ab26f
Magesh N <mageshnarayanaswamy5@gmail.com>
2023-10-20 21:14:01 +0530

    6commit

--- index.js
+++ index.js
@@ -4,9 +4,32 @@ const app = express();
 // Get the dynamic port from the environment variable
 const port = process.env.PORT || 3000;
 
-// Create a simple home page
+// Create a home page
 app.get('/', (req, res) => {
-  res.send('index.html');
+  res.send(`
+    <h1>Welcome to my website!</h1>
+    <p>Click on the links below to learn more about me.</p>
+    <ul>
+      <li><a href="/contact">Contact me</a></li>
+      <li><a href="/bio">My bio</a></li>
+    </ul>
+  `);
+});
+
+// Create a contact page
+app.get('/contact', (req, res) => {
+  res.send(`
+    <h1>Contact me</h1>
+    <p>My email address is john.doe@example.com.</p>
+  `);
+});
+
+// Create a bio page
+app.get('/bio', (req, res) => {
+  res.send(`
+    <h1>My bio</h1>
+    <p>I am a software engineer at Google.</p>
+  `);
 });
 
 // Start the server on the dynamic port
