4dc39c838049cd0646bf54b718fc4618dbe7b448
Magesh N <mageshnarayanaswamy5@gmail.com>
2023-10-20 20:28:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Get the dynamic port from the environment variable
+const port = process.env.PORT || 3000;
+
+// Create a simple home page
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server on the dynamic port
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
